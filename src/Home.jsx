300440fd1930923b2617587d70c218d6c089e2c6
Louise Marissal <marissal.louise@gmail.com>
2020-01-14 21:49:53 +0100

    correction

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <div className="homepage-container">
       <section>
-        <div className="img-container">
-          <div className="text">
+        <div className="content">
+          <div className="image-container">
             <img src="./img/IMG_0002.jpg" alt="avatar" />
+          </div>
 
+          <div className="text">
             <h1>Hello, i'm Louise.</h1>
             <p>Full Stack Junior Developer.</p>
           </div>
