2eb31d9bf885e870e0ec1c01d136a4b1b53371fb
Louise Marissal <marissal.louise@gmail.com>
2020-01-16 10:07:35 +0100

    alert msg envoyé

--- src/views/ContactForm.jsx
+++ src/views/ContactForm.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useRef } from "react";
 import layout from "./../CSS/layout.scss";
+import { Alert } from "reactstrap";
 import axios from "axios";
 
 const ContactForm = () => {
   const [formValues, setFormValues] = useState({});
+  const [dbMessage, setDbMessage] = useState("");
+  const [hideDbMessage, setHideDbMessage] = useState(true);
   const name = useRef();
   const email = useRef();
   const message = useRef();
@@ -22,9 +25,11 @@ const ContactForm = () => {
       .post(process.env.REACT_APP_BACKEND_URL + "/send", formValues)
       .then(response => {
         if (response.status === 200) {
-          alert("Message Sent.");
+          setDbMessage("Message envoyé");
+          setHideDbMessage(false);
         } else if (response.status === 500 || 503) {
-          alert("Message failed to send.");
+          setDbMessage("Message failed to send.");
+          setHideDbMessage(false);
         }
       })
       .catch(error => {
@@ -40,6 +45,14 @@ const ContactForm = () => {
         onChange={e => handleChange(e)}
         onSubmit={e => handleSubmit(e)}
       >
+        {!hideDbMessage ? (
+          <div className="alert alert-success" role="alert">
+            {dbMessage}
+          </div>
+        ) : (
+          ""
+        )}
+        {/* <Alert className="is-active alert-success">Message envoyé !</Alert> */}
         <h1>Me contacter</h1>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" ref={name} required />
